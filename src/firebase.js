ae952764299a6fd3845fafc1979325c0c7187d30
aniket-pageup <aniket@pageupsoft.com>
2020-09-10 23:19:59 +0530

    added-sdk

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCLlVi_Ga24AbSRHO_fAa9eNb6mg1oQZas",
+    authDomain: "whatsapp-mern-ea38a.firebaseapp.com",
+    databaseURL: "https://whatsapp-mern-ea38a.firebaseio.com",
+    projectId: "whatsapp-mern-ea38a",
+    storageBucket: "whatsapp-mern-ea38a.appspot.com",
+    messagingSenderId: "845123209262",
+    appId: "1:845123209262:web:ce3fc5bc7b07da6c0277bf",
+    measurementId: "G-ZXTRDSN4NR"
+  };
